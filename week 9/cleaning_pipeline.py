1a6d67ad34c5ddf2a5fa1894032fa36ffe409d7c
Juan Carlos <juanca.gc91@gmail.com>
2021-05-04 13:58:41 +0200

    transformation of categories Y and N into the pipeline file

--- week 9/cleaning_pipeline.py
+++ week 9/cleaning_pipeline.py
@@ -7,3 +7,13 @@ def cleaning(input_file_path, output_file_name):
 
     import pandas as pd
     data = pd.read_excel(str(input_file_path), sheet_name=1)
+
+    # transformation for variables with Y and N
+    data.replace(to_replace={'Y': 1, 'N': 0}, inplace=True)
+
+    """
+    Guys, write your code in between. The output file code (below) is the end of the function :)
+    """
+
+    # output file
+    data.to_csv(str(output_file_name), index=False)
