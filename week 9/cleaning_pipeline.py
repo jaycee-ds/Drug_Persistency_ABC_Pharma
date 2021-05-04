099b0329395892b8137ef412a21ef611b50363a8
GersonOM <sunstreaker.cynos@gmail.com>
2021-05-04 10:16:32 -0500

    transformation of categories and elimination of variables

--- week 9/cleaning_pipeline.py
+++ week 9/cleaning_pipeline.py
@@ -14,6 +14,17 @@ def cleaning(input_file_path, output_file_name):
     """
     Guys, write your code in between. The output file code (below) is the end of the function :)
     """
+    # Elimination of variables with more than 40% missing values
+    data = data.drop(columns = ['Risk_Segment_During_Rx',
+                                    'Tscore_Bucket_During_Rx',
+                                    'Change_T_Score',
+                                    'Change_Risk_Segment'])
+
+    # Transformation for variables with ">-2.5" and "<=-2.5"
+    data.replace(to_replace={'>-2.5': 1, '<=-2.5': 0}, inplace=True)
+
+    # Transformation for variables with "VLR_LR" and "HR_VHR"
+    data.replace(to_replace={'VLR_LR': 1, 'HR_VHR': 0}, inplace=True)
 
     # output file
     data.to_csv(str(output_file_name), index=False)
